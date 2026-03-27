9b91e0a3a34553c06e03d8a1677f145ecafc31dc
Jimwell Calma <Jimwell.Calma@hoolisoftware.com>
2026-03-27 14:30:07 +0800

    chore: update mobile API URL to Railway production 2

--- apps/mobile/src/constants/config.ts
+++ apps/mobile/src/constants/config.ts
@@ -3,7 +3,7 @@ export const API_BASE_URL = __DEV__
   : 'https://auxtion-production.up.railway.app/api/v1';
 
 export const SOCKET_URL = __DEV__
-  ? 'http://192.168.100.206:3000/api/v1'
+  ? 'http://192.168.100.206:3000'
   : 'https://auxtion-production.up.railway.app';
 
 export const SOCKET_NAMESPACE = '/auctions';
