28e13d2f9919ef5c6e37826ca3b1db4d387456db
Jimwell Calma <Jimwell.Calma@hoolisoftware.com>
2026-05-27 23:35:52 +0800

    feat: push notification to winner when auction item ends

--- apps/api/src/modules/bidding/bidding.module.ts
+++ apps/api/src/modules/bidding/bidding.module.ts
@@ -4,10 +4,11 @@ import { BiddingService } from './bidding.service';
 import { MaxBidsModule } from '../max-bids/max-bids.module';
 import { PaymentsModule } from '../payments/payments.module';
 import { OrdersModule } from '../orders/orders.module';
+import { NotificationsModule } from '../notifications/notifications.module';
 
 @Module({
-  imports: [MaxBidsModule, PaymentsModule, OrdersModule],
+  imports: [MaxBidsModule, PaymentsModule, OrdersModule, NotificationsModule],
   providers: [BiddingGateway, BiddingService],
-  exports: [BiddingService, BiddingGateway],
+  exports: [BiddingGateway],
 })
 export class BiddingModule {}
